1552024fd66ccdffee2a75e36c14be51a2e6202a
Francisco López <flopez_1384@hotmail.com>
2021-06-30 21:48:28 -0500

    [ADD] The complete functionality to calculate disscount and sending the result to the HTML via JavaScript

--- descuentos.js
+++ descuentos.js
@@ -16,3 +16,19 @@ function calcularPrecioConDescuento(precio, descuento) {
   const precioConDescuento = (precio * porcentajePrecioDescuento) / 100;
   return precioConDescuento;
 }
+
+function onClickButtonPriceDiscount() {
+  const inputPrice = document.getElementById("InputPrice");
+  const priceValue = inputPrice.value;
+  const inputDiscount = document.getElementById("InputDiscount");
+  const priceDiscount = inputDiscount.value;
+
+  const precioConDescuento = calcularPrecioConDescuento(
+    priceValue,
+    priceDiscount
+  );
+
+  const resultPrice = document.getElementById("ResultPrice");
+  //resultPrice.innerText = "El precio con descuento es $" + precioConDescuento;
+  resultPrice.innerText = `El precio con descuento es $${precioConDescuento}`;
+}
