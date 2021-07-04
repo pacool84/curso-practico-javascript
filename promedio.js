d3dfbbd34af091abeef8119d208d7d81e9ee6f51
Francisco López <flopez_1384@hotmail.com>
2021-07-04 11:14:39 -0500

    [ADD] The complete functions to calculate the average for a number list

--- promedio.js
+++ promedio.js
@@ -1,9 +1,29 @@
-const lista1 = [100, 200, 300, 500];
+//const lista1 = [100, 200, 300, 500];
 
-let sumaLista1 = 0;
+/* let sumaLista1 = 0;
 
 for (let i = 0; i < lista1.length; i++) {
   sumaLista1 = sumaLista1 + lista1[i];
 }
 
-console.log(sumaLista1);
+console.log("La suma de los valores en la lista es " + sumaLista1);
+
+const promedioLista1 = sumaLista1 / lista1.length;
+
+console.log("El promedio de los valores es " + promedioLista1); */
+
+function calcularMediaAritmetica(lista) {
+  let sumaLista = 0;
+
+  for (let i = 0; i < lista.length; i++) {
+    sumaLista = sumaLista + lista[i];
+  }
+
+  console.log("La suma de los valores en la lista es " + sumaLista);
+
+  const promedioLista = sumaLista / lista.length;
+
+  console.log("El promedio de la lista de valores es " + promedioLista);
+
+  return promedioLista;
+}
