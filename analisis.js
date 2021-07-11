406fcc7dc6a22e8356ce83d02d393a43bd83d028
Francisco López <flopez_1384@hotmail.com>
2021-07-11 11:23:03 -0500

    [ADD] The complete code to analize salaries

--- analisis.js
+++ analisis.js
@@ -1,47 +1,61 @@
+// Helpers
+function esPar(numerito) {
+  return (numerito % 2 === 0);
+}
+
+function calcularMediaAritmetica(lista) {
+  const sumaLista = lista.reduce(
+    function (valorAcumulado = 0, nuevoElemento) {
+      return valorAcumulado + nuevoElemento;
+    }
+  );
+  const promedioLista = sumaLista / lista.length;
+  return promedioLista;
+}
+
+// Calculadora de mediana
+function medianaSalarios(lista) {
+  const mitad = parseInt(lista.length / 2);
+
+  if (esPar(lista.length)) {
+    const personitaMitad1 = lista[mitad - 1];
+    const personitaMitad2 = lista[mitad];
+
+    const mediana = calcularMediaAritmetica([personitaMitad1, personitaMitad2]);
+    return mediana;
+  } else {
+    const personitaMitad = lista[mitad];
+    return personitaMitad;
+  }
+}
+
+// Mediana General
 const salariosCol = colombia.map(
-  function(persona) {
-    return persona.salario;
+  function (personita) {
+    return personita.salary;
   }
 );
 
 const salariosColSorted = salariosCol.sort(
-  function(salaryA, salaryB) {
+  function (salaryA, salaryB) {
     return salaryA - salaryB;
   }
 );
 
-function esPar(numero) {
- return (numero % 2 === 0)
-};
+const medianaGeneralCol = medianaSalarios(salariosColSorted);
 
-function calcularMediaAritmetica(lista) {
-  const sumaLista = lista.reduce(function (valorAcumulado = 0, nuevoElemento) {
-    return valorAcumulado + nuevoElemento;
-  });
+// Mediana del top 10%
+const spliceStart = (salariosColSorted.length * 90) / 100;
+const spliceCount = salariosColSorted.length - spliceStart;
 
-  console.log("La suma de los valores en la lista es " + sumaLista);
+const salariosColTop10 = salariosColSorted.splice(
+  spliceStart,
+  spliceCount,
+);
 
-  const promedioLista = sumaLista / lista.length;
+const medianaTop10Col = medianaSalarios(salariosColTop10);
 
-  console.log("El promedio de la lista de valores es " + promedioLista);
-
-  return promedioLista;
-}
-
-function medianaSalarios(lista) {
-  const mitad = parseInt(lista.lenght / 2);
-
-  if (esPar(lista.lenght)) {
-    const personaMitad1 = lista[mitad - 1];
-    const personaMitad2 = lista [mitad];
-
-    const mediana = calcularMediaAritmetica([personaMitad1, personaMitad2])
-    return mediana;
-
-  } else {
-    const personaMitad = lista[mitad]
-    return personaMitad;
-  }
-}
-
-console.log(medianaSalarios(salariosColSorted));
+console.log({
+  medianaGeneralCol,
+  medianaTop10Col,
+});
